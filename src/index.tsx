63999f850a3d97d913e5e8d7143ef4b9de47087f
seil_yoon <seil_yoon@tmax.co.kr>
2023-05-12 15:53:47 +0900

    [feat] add react-query setting

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,23 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { RecoilRoot } from "recoil";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <RecoilRoot>
-      <App />
-    </RecoilRoot>
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        {/* devtools */}
+        <ReactQueryDevtools initialIsOpen={true} />
+        <App />
+      </RecoilRoot>
+    </QueryClientProvider>
   </React.StrictMode>
 );
 
